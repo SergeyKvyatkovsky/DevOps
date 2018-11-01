c295be1503993786c966f4c41092d41f28099b42
moveton@tut.by <moveton@tut.by>
2018-11-01 04:14:03 +0300

    troubleshooting with power shell script

--- HomeWork/powershellScrpt.ps1
+++ HomeWork/powershellScrpt.ps1
@@ -6,12 +6,14 @@ $HTTP_Status = [int]$HTTP_Response.StatusCode
 
 If ($HTTP_Status -eq 200) {
     Write-Host "Site is OK!"
+....Stop-Process Start-Process -PassThru -FilePath $OctopusParameters["Octopus.Action[Deploy 1].Output.Package.InstallationDirectoryPath"] \musicstore.exe
 }
 Else {
     Write-Host "The Site may be down, please check!"
+    
 }
 
-# Finally, we clean up the http request by closing it.
 $HTTP_Response.Close()
+Stop-Process Start-Process -PassThru -FilePath $OctopusParameters["Octopus.Action[Deploy 1].Output.Package.InstallationDirectoryPath"] \musicstore.exe
 
 ping www.google.com -t
