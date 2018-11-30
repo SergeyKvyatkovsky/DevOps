72b98c90f2e25e86c460b31442c2e70530c9c11a
root <root@localhost.localdomain>
2018-11-29 21:15:00 -0500

    add Docker CE install and start Demon and add test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,15 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
 # This is an example test, replace it with your own test.
 describe port(5000), :skip do
   it { should_not be_listening }
+end
+
+describe package('Install Docker-ce') do
+  it { should be_installed}
+end
+
+describe service('docker') do
+  it { should be_enabled }
 end
