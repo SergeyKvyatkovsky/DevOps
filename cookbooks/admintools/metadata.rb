66a68ff737a054d1f235655cd087dcdc9ee1e053
moveton@tut.by <moveton@tut.by>
2018-12-12 08:56:22 +0300

    mod10 update

--- cookbooks/admintools/metadata.rb
+++ cookbooks/admintools/metadata.rb
@@ -3,6 +3,6 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 description 'Installs/Configures admintools'
 long_description 'Installs/Configures admintools'
-version '0.1.4'
+version '0.1.5'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
